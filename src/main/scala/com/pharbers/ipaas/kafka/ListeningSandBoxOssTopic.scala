ae982d4bd687fc696b6b4268eccda7a8e8b8ba9e
Faiz <891366402@qq.com>
2019-09-05 21:18:27 +0800

    明天抽离，结束后与前端对接口调试

--- src/main/scala/com/pharbers/ipaas/kafka/ListeningSandBoxOssTopic.scala
+++ src/main/scala/com/pharbers/ipaas/kafka/ListeningSandBoxOssTopic.scala
@@ -1,5 +1,7 @@
 package com.pharbers.ipaas.kafka
 
+import com.aliyun.oss.OSSClientBuilder
+import com.pharbers.ipaas.hdfs.HDFSUtil
 import com.pharbers.kafka.consumer.PharbersKafkaConsumer
 import com.pharbers.kafka.schema.ListeningSandBoxOss
 import org.apache.kafka.clients.consumer.ConsumerRecord
@@ -18,8 +20,28 @@ case class ListeningSandBoxOssTopic(topic: List[String], group: String) {
 	}
 	
 	def process(record: ConsumerRecord[String, ListeningSandBoxOss]): Unit = {
-		println(record.value().toString)
+		println(record.value())
 		
+		// Endpoint以杭州为例，其它Region请按实际情况填写。
+		val endpoint = "oss-cn-beijing.aliyuncs.com";
+		// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+		val accessKeyId = "LTAIEoXgk4DOHDGi"
+		val accessKeySecret = "x75sK6191dPGiu9wBMtKE6YcBBh8EI"
+		val bucketName = "pharbers-sandbox"
+		val objectName = record.value().get("Path").toString
+		
+		// 创建OSSClient实例。
+		val ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+		
+		
+		// ossObject包含文件所在的存储空间名称、文件名称、文件元信息以及一个输入流。
+		val ossObject = ossClient.getObject(bucketName, objectName);
+		
+		val dir = "/test/alex/temp2"
+		val remoteFile = dir + "/001test"
+		HDFSUtil.uploadStream2HDFS(ossObject.getObjectContent, remoteFile)
+		// 关闭OSSClient。
+		ossClient.shutdown();
 	}
 	
 }
